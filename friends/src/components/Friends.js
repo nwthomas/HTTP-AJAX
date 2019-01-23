4e95d1d3f208cc0fb8c7744bd6b1f03c22c0d472
Nathan Thomas <nwthomas@me.com>
2019-01-23 12:48:33 -0800

    Finish building Friend.js component

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Friends = () => {
-  return <div />;
+const Friends = props => {
+  return (
+    <div className="friend">
+      <h2 className="friend__name">{props.friend.name}</h2>
+      <p className="friend__age">{props.friend.age}</p>
+      <p className="friend__email">{props.friend.email}</p>
+    </div>
+  );
 };
 
 export default Friends;
