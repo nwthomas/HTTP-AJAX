b6c14f696438ef10594b907ae9cf91b524bd3bfd
Nathan Thomas <nwthomas@me.com>
2019-01-23 21:51:16 -0800

    Implement NavLinks in navbar from React Router

--- friends/src/components/NavbarComponent/Navbar.js
+++ friends/src/components/NavbarComponent/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = props => {
   return (
     <div className="navbar">
-      <p>Lambda Friends List</p>
+      <div><NavLink exact to="/">Home</NavLink><NavLink to="/form">Form</NavLink></div>
       <input
         className="search__input"
         name="searchInput"
